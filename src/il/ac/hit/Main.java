004742700e5ed6ac033f50c42386883b480a9dd4
sigalit leybman <sigalitleybman@gmail.com>
2022-01-20 15:11:52 +0200

    refactored

--- src/il/ac/hit/Main.java
+++ src/il/ac/hit/Main.java
@@ -19,24 +19,6 @@ public class Main
 
     public static void main(String args[]) throws CostManagerException
     {
-//       try
-//        {
-//            Class.forName(driverFullQualifiedName);
-//            Connection connection = DriverManager.getConnection(connectionStringToDB, dataBaseUserName, dataBasePassword);
-//            Statement statement = connection.createStatement();
-//
-//
-////            ResultSet rs = statement.executeQuery("select * from categorieslist");
-////            while(rs.next()){
-////                System.out.println("ID:" + rs.getInt(1) + "Category Name: " + rs.getString(2));
-////            }
-//        }
-//        catch (Exception e)
-//        {
-//            System.out.println("Unable to connect to the server");
-//        }
-
-
         Model model = new CostManagerModel();
         ViewModel viewModel = new CostManagerViewModel();
         View view = new ViewManager();
